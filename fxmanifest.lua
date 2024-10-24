9628849c141c73a1c3038c2f4d85d6798d9a7b29
Macho <155906434+machoLTD@users.noreply.github.com>
2024-10-24 13:34:15 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'https://github.com/machoLTD'
+description 'Simple Script to spawn Cars'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'car.lua',
+    
+}
+
